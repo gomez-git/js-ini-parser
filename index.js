3dc3ed379f722a63a69dbf01c077dc0027d347f6
gomez-git <78104957+gomez-git@users.noreply.github.com>
2022-05-06 23:16:17 +0300

    Refactor main file
    Add prepareData function, rename isValidKey to isForbiddenKey, change default export

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const validateValue = (value) => {
+const prepareData = (data) => data
+  .split('\n')
+  .filter((line) => line.match(/^[^;#\s]/))
+  .reduce((acc, line) => {
+    if (line.startsWith('[')) {
+      acc[acc.length] = [line];
+    } else {
+      acc[acc.length - 1].push(line);
+    }
+    return acc;
+  }, []);
+
+const formatValue = (value) => {
   switch (true) {
     case value === 'true':
       return true;
     case value === 'false':
       return false;
-    case value === '':
-      return value;
     case value === 'null':
       return null;
-    case !Number.isNaN(Number(value)):
+    case !Number.isNaN(parseInt(value, 10)):
       return Number(value);
     default:
       return value;
   }
 };
 
-const isValidKey = (key) => key !== '__proto__' && key !== 'constructor' && key !== 'prototype';
+const forbiddenKeys = ['constructor', '__proto__', 'prototype'];
+const isForbiddenKey = (key) => forbiddenKeys.find((forbiddenKey) => forbiddenKey === key);
 
 const parse = (data, object) => {
   const obj = object ?? {};
@@ -24,57 +35,47 @@ const parse = (data, object) => {
   const callback = (acc, property, _i, arr) => {
     if (property.startsWith('[')) {
       const nodes = property.slice(1).split('.');
+
       if (nodes.length > 1) {
         const key = nodes.shift() || previousNode;
-        if (!isValidKey(key)) {
+        if (isForbiddenKey(key)) {
           arr.splice(1);
           return acc;
         }
         obj[key] = {
           ...obj[key],
-          ...parse([`[${nodes.join('.')}`, ...arr.slice(1)].join('\n'), obj[key]),
+          ...parse([[`[${nodes.join('.')}`, ...arr.splice(1)]], obj[key]),
         };
-        arr.splice(1);
 
         return key;
       }
+
       const key = property.slice(1, property.length - 1);
-      if (!isValidKey(key)) {
+      if (isForbiddenKey(key)) {
         arr.splice(1);
+        previousNode = key;
         return acc;
       }
       obj[key] = obj[key] ?? {};
 
       return key;
     }
+
     const [key, value] = property.split('=');
-    if (!isValidKey(key)) {
-      return acc;
-    }
     const trimmedKey = key.trim();
     const trimmedValue = value.trim();
-
-    obj[acc][trimmedKey] = obj[acc][trimmedKey] ?? '';
-    obj[acc][trimmedKey] = validateValue(trimmedValue);
+    if (isForbiddenKey(trimmedKey)) {
+      return acc;
+    }
+    obj[acc][trimmedKey] = obj[acc][trimmedKey] ?? formatValue(trimmedValue);
     previousNode = acc;
 
     return acc;
   };
 
-  data
-    .split('\n')
-    .filter((line) => line !== '' && !line.startsWith(';'))
-    .reduce((acc, line) => {
-      if (line.startsWith('[')) {
-        acc[acc.length] = [line];
-        return acc;
-      }
-      acc[acc.length - 1].push(line);
-      return acc;
-    }, [])
-    .forEach((leaf) => leaf.reduce(callback, ''));
+  data.forEach((leaf) => leaf.reduce(callback, ''));
 
   return obj;
 };
 
-export default parse;
+export default (data) => parse(prepareData(data));
